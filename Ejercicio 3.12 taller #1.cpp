a41f95a2a95dd9016273bba1ed4de50bedaaaf18
JuanMartinez-ctrl <jmartinez064@uan.edu.co>
2026-09-17 09:42:44 -0500

    Add files via upload

---
+++ Ejercicio 3.12 taller #1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int numeroEntero = 10;
+    float numeroDecimal = 3.1416;
+    char letra = 'A';
+
+    cout << "Salida continua:" << endl;
+    cout << "Texto " << numeroEntero << " " << numeroDecimal << " " << letra << endl << endl;
+
+    cout << "Salida con saltos de linea:" << endl;
+    cout << "Entero: " << numeroEntero << endl;
+    cout << "Decimal: " << numeroDecimal << endl;
+    cout << "Letra: " << letra << endl << endl;
+
+    cout << "Salida con espacios simples:" << endl;
+    cout << "ID: " << numeroEntero << " Valor: " << numeroDecimal << " Letra: " << letra << endl;
+
+
+}
